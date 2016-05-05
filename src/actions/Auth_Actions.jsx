545b69c1d9e7338b29dd6bb84ce743d112b15d06
tmpace <tmpace11@gmail.com>
2016-05-05 16:49:12 -0700

    (chore) Clean up Auth_Actions

--- src/actions/Auth_Actions.jsx
+++ src/actions/Auth_Actions.jsx
@@ -1,7 +1,6 @@
 import Auth0Lock from 'auth0-lock';
 
 /* POSSIBLE LOGIN ACTIONS */
-
 export const SHOW_LOCK = 'SHOW_LOCK';
 export const LOCK_SUCCESS = 'LOCK_SUCCESS';
 export const LOCK_ERROR = 'LOCK_ERROR';
@@ -9,8 +8,8 @@ export const LOCK_ERROR = 'LOCK_ERROR';
 // Action creator for showing Lock
 function showLock() {
   return {
-    type: SHOW_LOCK
-  }
+    type: SHOW_LOCK,
+  };
 }
 
 // Action creator for login success
@@ -18,34 +17,35 @@ function lockSuccess(profile, token) {
   return {
     type: LOCK_SUCCESS,
     profile,
-    token
-  }
+    token,
+  };
 }
 
 // Action creator for login error
 function lockError(err) {
   return {
     type: LOCK_ERROR,
-    err
-  }
+    err,
+  };
 }
 
-// This function opens the lock widget and dispatches actions along the way
+// This function opens lock widget and dispatches actions
 export function login() {
   const lock = new Auth0Lock('7knLLEidEQiUBihGrdb8GKVtNnfPF1A5', 'comparable.auth0.com');
   return dispatch => {
     lock.show((err, profile, token) => {
-      // If there was an error logging in, dispatch the lockErr function
+      // If we receive an error, we dispatch the lockError action
       if (err) {
         dispatch(lockError(err));
         return;
       }
-      console.log(profile);
+
+      // If user logs in successfully, we add profile and id to localStorage, dispatch lockSuccess
       localStorage.setItem('profile', JSON.stringify(profile));
       localStorage.setItem('id_token', token);
       dispatch(lockSuccess(profile, token));
     });
-  }
+  };
 }
 
 /* POSSIBLE LOGOUT ACTIONS */
@@ -57,16 +57,16 @@ function requestLogout() {
   return {
     type: LOGOUT_REQUEST,
     isFetching: true,
-    isAuthenticated: true
-  }
+    isAuthenticated: true,
+  };
 }
 
 function receiveLogout() {
   return {
     type: LOGOUT_SUCCESS,
     isFetching: false,
-    isAuthenticated: false
-  }
+    isAuthenticated: false,
+  };
 }
 
 export function logoutUser() {
@@ -74,5 +74,5 @@ export function logoutUser() {
     dispatch(requestLogout());
     localStorage.removeItem('id_token');
     dispatch(receiveLogout());
-  }
+  };
 }
