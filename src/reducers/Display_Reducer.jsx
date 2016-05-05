538e39b49996016204d40d17feb297b8961a692c
Justin <justinminh225@gmail.com>
2016-05-05 12:32:14 -0700

    (style) removed rebase conflict

--- src/reducers/Display_Reducer.jsx
+++ src/reducers/Display_Reducer.jsx
@@ -5,11 +5,7 @@ export default function(state = null, action) {
   case JOB_DISPLAY:
     console.log('logging from display_reducer');
     console.log(action.payload);
-<<<<<<< 02ff921c817fdc5bac80f9e45e26bb926e9f469a
     return action.payload;
-=======
-    return {display: action.payload};
->>>>>>> (feat)
   }
 
   return state;
