b974af15f6e962197cb4dfbd5da7fffcd1b4c6c3
tmpace <tmpace11@gmail.com>
2016-05-09 11:20:21 -0700

    (fix) Change variable names in offerDisplay

--- src/components/OfferDisplay.jsx
+++ src/components/OfferDisplay.jsx
@@ -3,17 +3,17 @@ import React, { Component } from 'react';
 export default class OfferDisplay extends Component {
   render() {
     // Grab the offer property from the props
-    const { offer } = this.props.data;
+    const { userOffer } = this.props.data;
 
     // Render the job offer information on the page
     return (
       <div className="row offer-form">
         <div className="col-md-10 col-md-offset-1">
-          {offer &&
+          {userOffer &&
             <ul className="displayList">
-              <li className="text-center offerTitle">{offer.title}</li>
+              <li className="text-center offerTitle">{userOffer.title}</li>
               <li className="text-center">
-                Location: {offer.location} | Salary: {offer.salary} | Equity: {offer.equity}
+                Location: {userOffer.location} | Salary: {userOffer.salary} | Equity: {userOffer.equity}
               </li>
             </ul>}
         </div>
