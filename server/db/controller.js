fe228155d1a032f7128e474d01a8b989d6b1760d
Justin <justinminh225@gmail.com>
2016-05-09 20:07:54 -0700

    (feat) convert db methods to use async

--- server/db/controller.js
+++ server/db/controller.js
@@ -3,25 +3,23 @@ var Offer = require('./mongo');
 module.exports = {
   getOffers: function(req, res) {
     console.log('in getOffer');
-    Offer.find(function(err, data) {
-      if (err)  {
-        res.status(200).send('cannot find data');
-      } else {
+    Offer.find().then(function(data) {
+      if (data)  {
         res.status(200).send(data);
+      } else {
+        res.status(200).send('cannot find data');
       }
-    })
+    });
   },
   createOffer: function(req, res) {
-    console.log('in createOffer');
     var offer = new Offer(req.body);
-    offer.save(function(err, data) {
-      if (err) {
-        res.status(302).send('error occured')
-      } else {
-        console.log('success');
+    offer.save().then(function(data) {
+      if (data) {
+        console.log('success post');
         res.status(302).send(data);
+      } else {
+        res.status(302).send('error occured')
       }
-    })
-    // res.status(302).send(req.body);
+    });
   }
 }
