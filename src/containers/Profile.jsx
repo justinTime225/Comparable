07a89330d225b901c086e08acbac8a683c590ad6
tmpace <tmpace11@gmail.com>
2016-05-11 12:02:25 -0700

    (feat) Add conditional to offer slider

--- src/containers/Profile.jsx
+++ src/containers/Profile.jsx
@@ -1,36 +1,44 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { getOffer } from '../actions/Profile_Offer';
-export default class Profile extends Component {
+import OfferSlider from '../components/OfferSlider';
+
+
+class Profile extends Component {
   componentWillMount() {
-    this.props.getOffer();
+    const { getOffers } = this.props;
+    getOffers();
   }
+
   render() {
-    const offerItems = this.props.profile.data.map((offer) => {
-      console.log(offer._id);
-      return (
-        <li key={offer._id}>
-          <h2>{offer.title}</h2>
-          <p>{offer.location}</p>
-          <p>{offer.salary}</p>
-          <p>{offer.equity}</p>
-        </li>
-      )
-    })
+    let profileData = this.props.profile.data;
+
     return (
-      <ul>{offerItems}</ul>
+      <div>
+      {profileData &&
+          <OfferSlider profileData={profileData}></OfferSlider> ||
+      }
+      </div>
     );
   }
 
 }
 
 function mapStateToProps(state) {
-  
+
   const { profile } = state;
   return {
-    profile
+    profile,
   };
 }
 
-export default connect(mapStateToProps, {getOffer})(Profile);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getOffers: () => {
+      dispatch(getOffer());
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
