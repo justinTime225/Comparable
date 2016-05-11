7f7f340af25ad89e2f7c2df68c40e521f7aab1a8
Justin <justinminh225@gmail.com>
2016-05-11 12:15:41 -0700

    created action function for skill tags

--- src/actions/Skills_Actions.jsx
+++ src/actions/Skills_Actions.jsx
@@ -2,8 +2,12 @@ import axios from 'axios';
 export const SKILLS_ACTION = "SKILLS_ACTION";
 export function getSkills(data) {
   console.log('inside getSkills');
-  console.log(data);
-  var request = axios.get('/api/skills', {title: data.title})
+  console.log(data.title);
+  var request = axios.get('/api/skills', {
+    params: {
+      title: data.title,
+    },
+  })
   console.log(request);
 
   return {
