cca1c576a80c2d45b234d0d3803d369eb283c966
tmpace <tmpace11@gmail.com>
2016-05-13 16:40:04 -0700

    (chore) Remove unnecessary console logs

--- src/reducers/ProfileOffer_Reducer.jsx
+++ src/reducers/ProfileOffer_Reducer.jsx
@@ -3,7 +3,6 @@ import { PROFILE_OFFER } from '../actions/Profile_Offer';
 export default function (state = [], action) {
   switch(action.type) {
     case PROFILE_OFFER:
-      console.log(action.payload);
       
       return action.payload;
   }
