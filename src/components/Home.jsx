293b41abffe0be1bbe6f9c9d54a48035738f4a11
Ryan Haase <ryandhaase@gmail.com>
2016-05-12 20:32:21 -0400

    (enhancement) Update landing page design:
    * Break into 3 sections (hero, mid, lower)
    * Update marketing language, button hover text, and spacing for hero section
    * Display marketing GIF in mid-section
    * Replace background with compressed image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,19 +4,31 @@ export default class Home extends Component {
   render() {
 
     return (
-      <div className="container" id="home">
-        <div className="row section1 vertical-center">
-          <div className="col-md-10 col-md-offset-1">
-            <div className="jumbotron">
-              <h1>Comparable</h1>
+      <div className="container-fluid" id="home">
+        <div className="row hero-section vertical-center">
+          <div className="col-md-6 col-md-offset-3">
+            <h1>Comparable</h1>
+            <br/>
+            <h5>See how your job offer stacks up...</h5>
+            <h5>With the most powerful job comparison tool on the market!</h5>
+            <br/>
+            <p><a className="btn btn-primary btn-lg"><span>Get Started</span></a></p>
+          </div>
+        </div>
+        <div className="row mid-section vertical-center">
+          <div className="col-md-6 col-md-offset-3">
+            <img alt="Offer GIF" src="https://s3.amazonaws.com/comparable/ezgif.com-gif-maker.gif" />
+          </div>
+        </div>
+        <div className="row lower-section vertical-center">
+          <div className="col-md-6 col-md-offset-3">
 
-              <p>See how your offer stacks up today!</p>
-
-              <p><a className="btn btn-primary btn-lg">Learn more</a></p>
-            </div>
+            <p><a className="btn btn-primary btn-lg">Learn More</a></p>
           </div>
         </div>
       </div>
     );
   }
 };
+
+// TODO: Remove verticle-align from CSS
